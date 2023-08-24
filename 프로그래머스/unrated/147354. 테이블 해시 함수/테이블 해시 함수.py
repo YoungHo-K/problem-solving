622483012641b6166efb1ddf6019d02601349ce7
Youngho Kim <130723311+YoungHo-K@users.noreply.github.com>
2023-08-24 21:01:31 +0900

    [unrated] Title: 테이블 해시 함수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.py
+++ 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.py
@@ -1,31 +1,15 @@
-import heapq
 
 
 def solution(data, col, row_begin, row_end):
-    # if len(data) == 1:
-    #     return sum(data[0]) ^ sum(data[0])
-        
-    sorted_data = list()
-    for row in data:
-        pivot = col - 1        
-        new_data = [row[pivot], -row[0]] + row[1: pivot] + row[pivot + 1:] 
-        
-        heapq.heappush(sorted_data, new_data)
-
-    index = 0
-    answer = None
-    while sorted_data and (index < row_end):
-        data = heapq.heappop(sorted_data)
-        
-        index += 1
-        if index < row_begin:
-            continue
-                
-        data[1] *= -1
-        mod_sum = 0
-        for val in data:
-            mod_sum += val % index
-
-        answer = mod_sum if answer is None else answer ^ mod_sum
+    data.sort(key=lambda x: (x[col - 1], -x[0]))
+    data = data[row_begin - 1: row_end]
     
+    answer = 0
+    for index, row in zip(range(row_begin, row_end + 1), data):
+        mod_sum = 0
+        for val in row:
+            mod_sum += val % index
+        
+        answer = answer ^ mod_sum
+
     return answer
