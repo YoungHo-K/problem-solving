8a9e43fa36271f3132b63f7c1d4fb393915c076e
Youngho Kim <130723311+YoungHo-K@users.noreply.github.com>
2024-06-13 22:04:17 +0900

    Time: 3067 ms (26.03%), Space: 16.6 MB (75.65%) - LeetHub

--- LeetCode/Medium/0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ LeetCode/Medium/0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -2,29 +2,32 @@
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if len(s) == 1:
-            return s
-
+        n = len(s)
+        
         answer = None
-        for right_pivot in range(0, len(s) - 1, 1):
-            left_pivot = right_pivot
-
-            for index in range(left_pivot, len(s), 1):
-                if s[right_pivot] != s[index]:
-                    break
+        for length in range(n, 0, -1):
+            
+            for index in range(0, n - length + 1):
+                substring = s[index: index + length]
                 
-                left_pivot = index
-
-            move = min(right_pivot, len(s) - left_pivot - 1)
-            for _ in range(0, move, 1):
-                if s[right_pivot - 1] != s[left_pivot + 1]:
+                left = 0
+                right = length - 1
+                
+                is_palindromic = True
+                while left < right:
+                    if substring[left] != substring[right]:
+                        is_palindromic = False
+                        break
+                    
+                    left += 1
+                    right -= 1
+                                    
+                if is_palindromic:
+                    answer = substring
                     break
-
-                right_pivot -= 1
-                left_pivot += 1
-
-            subanswer = s[right_pivot: left_pivot + 1]
-            if (answer is None) or (len(answer) < len(subanswer)):
-                answer = subanswer
-
+            
+            if answer is not None:
+                break
+        
         return answer
+        
