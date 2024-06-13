33d8600bce4edabe74549bcb07efb84156f2fe3a
Youngho Kim <130723311+YoungHo-K@users.noreply.github.com>
2024-06-13 21:49:22 +0900

    Time: 451 ms (15.40%), Space: 48.2 MB (8.03%) - LeetHub

--- LeetCode/Medium/0647-palindromic-substrings/0647-palindromic-substrings.py
+++ LeetCode/Medium/0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,13 +1,31 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         n = len(s)
+        dp = [[None] * n for _ in range(n)]
+        
+        def is_palindromic(start, end):
+            if end <= start:
+                return True
+            
+            if dp[start][end] is not None:
+                return dp[start][end]
+
+            if s[start] == s[end]:
+                dp[start][end] = is_palindromic(start + 1, end - 1)
+            
+            else:
+                dp[start][end] = False
+            
+            return dp[start][end] 
+
         
         answer = 0
-        for length in range(1, n + 1):
-            for index in range(0, n - length + 1):
-                substring = s[index: index + length]
-                
-                if substring == substring[::-1]:
+        for i in range(0, n):
+            for j in range(i, n):
+                if is_palindromic(i, j):
                     answer += 1
     
         return answer
+
+    
+        
