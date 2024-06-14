dd78bbab50fcbbd236b0492c1b7b1fd0eb6d6293
Youngho Kim <130723311+YoungHo-K@users.noreply.github.com>
2024-06-14 22:38:38 +0900

    Time: 460 ms (68.28%), Space: 35.2 MB (35.20%) - LeetHub

---
+++ LeetCode/Medium/0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -0,0 +1,30 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        n = len(s)
+                   
+        answer = []
+        substrings = []
+
+        def dfs(start):
+            if start == n:
+                answer.append(substrings.copy())                
+                return
+            
+            for end in range(start, n):
+                substr = s[start: end + 1]
+                
+                if substr == substr[::-1]:
+                    substrings.append(substr)
+                    dfs(end + 1)
+                    substrings.pop()
+            
+        for index in range(0, n):
+            substr = s[: index + 1]
+            
+            if substr == substr[::-1]:    
+                substrings.append(substr)
+                dfs(index + 1)
+                substrings.pop()
+                
+        return answer
+    
