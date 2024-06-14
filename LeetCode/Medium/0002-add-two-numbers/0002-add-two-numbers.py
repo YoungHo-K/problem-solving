bf1abcc96587f29f6b76adaee3490a83377752b9
Youngho Kim <130723311+YoungHo-K@users.noreply.github.com>
2024-06-14 20:42:16 +0900

    Time: 47 ms (90.77%), Space: 16.6 MB (36.63%) - LeetHub

--- LeetCode/Medium/0002-add-two-numbers/0002-add-two-numbers.py
+++ LeetCode/Medium/0002-add-two-numbers/0002-add-two-numbers.py
@@ -7,10 +7,10 @@
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        sum_value = 0
-
         root = ListNode()
         node = root
+        sum_value = 0
+        
         while l1 or l2:
             if l1:
                 sum_value += l1.val
@@ -19,13 +19,13 @@ class Solution:
             if l2:
                 sum_value += l2.val
                 l2 = l2.next
-            
-            node.next = ListNode(val=sum_value % 10, next=None)
-            sum_value = sum_value // 10
 
+            node.next = ListNode(val=sum_value % 10)                
             node = node.next
-
+            
+            sum_value //= 10
+            
         if sum_value != 0:
-            node.next = ListNode(val=sum_value, next=None)
+            node.next = ListNode(val=sum_value)
 
         return root.next
