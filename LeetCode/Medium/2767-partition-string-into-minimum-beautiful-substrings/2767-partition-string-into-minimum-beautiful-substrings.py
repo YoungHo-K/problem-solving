5fdf23b9b33503a2d42b183c7d112fabfae8fb03
Youngho Kim <130723311+YoungHo-K@users.noreply.github.com>
2024-06-19 22:23:38 +0900

    Time: 46 ms (57.83%), Space: 16.6 MB (67.39%) - LeetHub

--- LeetCode/Medium/2767-partition-string-into-minimum-beautiful-substrings/2767-partition-string-into-minimum-beautiful-substrings.py
+++ LeetCode/Medium/2767-partition-string-into-minimum-beautiful-substrings/2767-partition-string-into-minimum-beautiful-substrings.py
@@ -5,23 +5,22 @@ class Solution:
     def minimumBeautifulSubstrings(self, s: str) -> int:
         n = len(s)
         
-        answer = [None]
-        def dfs(start, depth):
+        def dfs(start):
             if n <= start:
-                if (answer[0] is None) or (depth < answer[0]):
-                    answer[0] = depth
-                
-                return
+                return 0
             
+            answer = float("inf")
             if s[start] != "0":
                 for end in range(start, n):
                     value = int(s[start: end + 1], 2)
                     while value % 5 == 0:
                         value //= 5
-                                        
-                    if value == 1:
-                        dfs(end + 1, depth + 1)
-
-        dfs(0, 0)            
                         
-        return -1 if answer[0] is None else answer[0]
+                    if value == 1:
+                        answer = min(answer, 1 + dfs(end + 1))
+                        
+            return answer
+        
+        answer = dfs(0)
+        return -1 if answer == float("inf") else answer
+        
