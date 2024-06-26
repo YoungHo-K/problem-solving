b2708cb055f62006a34248a8c6d3bdb0ca6b11d2
Youngho Kim <130723311+YoungHo-K@users.noreply.github.com>
2024-06-26 23:07:03 +0900

    Time: 40 ms (19.15%), Space: 16.5 MB (45.77%) - LeetHub

---
+++ LeetCode/Medium/0165-compare-version-numbers/0165-compare-version-numbers.py
@@ -0,0 +1,30 @@
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
+        answer = 0
+        
+        version1 = version1.split(".")
+        version2 = version2.split(".")
+        n, m = len(version1), len(version2)
+        
+        if n < m:
+            version1 += ["0"] * (m - n)
+                    
+        if m < n:
+            version2 += ["0"] * (n - m)
+        
+        print(version1)
+        print(version2)
+        
+        for index in range(0, max(n, m)):
+            value_1 = int(version1[index])
+            value_2 = int(version2[index])
+
+            if value_1 < value_2:
+                answer = -1
+                break
+            
+            if value_1 > value_2:
+                answer = 1
+                break
+                        
+        return answer
