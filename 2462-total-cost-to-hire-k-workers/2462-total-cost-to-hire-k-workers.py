9c749b17d98a05856308ec785e7905e41392b269
Youngho Kim <130723311+YoungHo-K@users.noreply.github.com>
2024-08-08 11:48:30 +0900

    Time: 600 ms (65.88%), Space: 27.1 MB (58.49%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.py
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.py
@@ -3,47 +3,35 @@ import heapq
 
 class Solution:
     def totalCost(self, costs: List[int], k: int, candidates: int) -> int:
-        costs = [(cost, index) for index, cost in enumerate(costs)]
+        n = len(costs)
         
+        left_pivot = candidates - 1
         left_heap = costs[:candidates]
-        right_heap = costs[-candidates:]
         heapq.heapify(left_heap)
+        
+        right_pivot = n - candidates
+        right_heap = [costs[index] for index in range(right_pivot, n) if left_pivot < index]
         heapq.heapify(right_heap)
         
-        n = len(costs)
-        left_pivot = candidates
-        right_pivot = n - candidates - 1
-                
-        total_cost = 0
-        used = set()
+        total_cost = 0        
         for _ in range(k):
-            left_value, left_index = heapq.heappop(left_heap) if len(left_heap) > 0 else (float('inf'), -1)
-            right_value, right_index = heapq.heappop(right_heap) if len(right_heap) > 0 else (float('inf'), -1)
+            left_value = left_heap[0] if len(left_heap) > 0 else float('inf')
+            right_value = right_heap[0] if len(right_heap) > 0 else float('inf')
             
             if left_value <= right_value:
                 total_cost += left_value
-                used.add(left_index)
-
-                if (left_pivot < n) and (left_pivot not in used):
-                    heapq.heappush(left_heap, costs[left_pivot])
+                heapq.heappop(left_heap)
+                
+                if left_pivot + 1 < right_pivot:
+                    heapq.heappush(left_heap, costs[left_pivot + 1])
                     left_pivot += 1
                 
-                if left_index == right_index:
-                    if (right_pivot >= 0) and (right_pivot not in used):
-                        heapq.heappush(right_heap, costs[right_pivot])
-                        right_pivot -= 1
-                else:
-                    heapq.heappush(right_heap, (right_value, right_index))
-            
             else:
                 total_cost += right_value
-                used.add(right_index)
+                heapq.heappop(right_heap)
                 
-                if (right_pivot >= 0) and (right_pivot not in used):
-                    heapq.heappush(right_heap, costs[right_pivot])
+                if left_pivot < right_pivot - 1:
+                    heapq.heappush(right_heap, costs[right_pivot - 1])
                     right_pivot -= 1
-                
-                heapq.heappush(left_heap, (left_value, left_index))
-                
+                    
         return total_cost
-            
