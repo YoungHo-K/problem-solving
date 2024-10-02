b625782c186b8473b9bdf246cb09962cf3204fe5
Youngho Kim <130723311+YoungHo-K@users.noreply.github.com>
2024-10-02 22:45:30 +0900

    Time: 35 ms (58.88%), Space: 16.6 MB (16.95%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,7 +1,5 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        directions = [(0, 1), (1, 0)]
-        
         dp = [[0] * n for _ in range(m)]
         for x in range(0, m):
             dp[x][0] = 1
