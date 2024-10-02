f05261cf8d19060a0cb9a0e056db0053d7568d29
Youngho Kim <130723311+YoungHo-K@users.noreply.github.com>
2024-10-02 22:33:38 +0900

    Time: 759 ms (21.93%), Space: 64.9 MB (28.35%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -3,5 +3,20 @@ from itertools import combinations
 
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        return list(map(list, combinations(range(1, n + 1), k)))
-    
+        # return list(map(list, combinations(range(1, n + 1), k)))
+        answer = []
+        
+        visited = set()
+        def dfs(start):
+            if len(visited) == k:
+                answer.append(list(visited))
+                return
+            
+            for value in range(start + 1, n + 1):
+                visited.add(value)
+                dfs(value)
+                visited.remove(value)
+        
+        dfs(0)
+        
+        return answer
